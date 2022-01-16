3cac43f0a7e85726fda3478792ded2f842d24175
TheDarkStrix <thedarkstrix@gmail.com>
2022-01-16 23:35:32 +0530

    Add dynamic 404 pages

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -40,6 +40,22 @@ app.get("/weather", (req, res) => {
   });
 });
 
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    error: "Help article not found",
+    title: "404",
+    name: "Manoj",
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("404", {
+    error: "Page not found.",
+    title: "404",
+    name: "Manoj",
+  });
+});
+
 app.listen(3000, () => {
   console.log("Server is up on port 3000");
 });
