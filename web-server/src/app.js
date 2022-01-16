49ba81397333f3358f117dff62d2dc8222ae3e44
TheDarkStrix <thedarkstrix@gmail.com>
2022-01-16 22:55:10 +0530

    HandleBars: Initial dynamic content render

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -7,6 +7,21 @@ const publicDirPath = path.join(__dirname, "../public");
 app.set("view engine", "hbs");
 app.use(express.static(publicDirPath));
 
+app.get("", (req, res) => {
+  res.render("index", { title: "Weather App", name: "Manoj" });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About page", name: "Manoj" });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help page",
+    message: "This is a help message",
+  });
+});
+
 app.get("/weather", (req, res) => {
   res.send({
     forecast: "it is snowing",
