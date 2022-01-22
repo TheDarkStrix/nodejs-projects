2b7ec78aac14962a2bac9e883b35f03d1a027b4f
TheDarkStrix <thedarkstrix@gmail.com>
2022-01-22 18:26:10 +0530

    Fetch weather details via API [Frontend]

---
+++ web-server/public/js/script.js
@@ -0,0 +1,21 @@
+const fetchWeather = (location) => {
+  fetch(`http://localhost:3000/weather?address=${location}`)
+    .then((response) => {
+      response.json().then((data) => {
+        if (data.error) return (result.innerHTML = data.error);
+        result.innerHTML = `<p>${data.location}</p><p>${data.forecast}</p>`;
+      });
+    })
+    .catch((error) => {
+      result.innerHTML = error;
+    });
+};
+
+const weatherForm = document.querySelector("form");
+const weatherLocation = document.querySelector("input");
+const result = document.querySelector("#result");
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  result.textContent = "Loading...";
+  fetchWeather(weatherLocation.value);
+});
